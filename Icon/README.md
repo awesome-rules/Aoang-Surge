7a732cfe4cabe340fb2f8cd64a51c2c472f2e07d
Aoang <aoang@x2oe.com>
2025-06-16 21:08:43 +0800

    format

--- Icon/README.md
+++ Icon/README.md
@@ -1,5 +1,4 @@
-# Icon 
-
+# Icon
 
 ### Source
 
--- src/ad_guard.ts
+++ src/ad_guard.ts
@@ -12,24 +12,24 @@ export default async function build() {
   const data = await parse();
   const set = new Set(data);
 
-  set.add('.log.aliyuncs.com')
+  set.add(".log.aliyuncs.com");
 
-  set.add('cls.tencentcloudapi.com')
-  set.add('cls.ap-guangzhou.tencentcloudapi.com')
-  set.add('cls.ap-shanghai.tencentcloudapi.com')
-  set.add('cls.ap-beijing.tencentcloudapi.com')
-  set.add('cls.ap-chengdu.tencentcloudapi.com')
-  set.add('cls.ap-chongqing.tencentcloudapi.com')
-  set.add('cls.ap-hongkong.tencentcloudapi.com')
-  set.add('cls.ap-singapore.tencentcloudapi.com')
-  set.add('cls.ap-bangkok.tencentcloudapi.com')
-  set.add('cls.ap-seoul.tencentcloudapi.com')
-  set.add('cls.ap-tokyo.tencentcloudapi.com')
-  set.add('cls.na-ashburn.tencentcloudapi.com')
-  set.add('cls.na-siliconvalley.tencentcloudapi.com')
-  set.add('cls.eu-frankfurt.tencentcloudapi.com')
-  set.add('cls.ap-shanghai-fsi.tencentcloudapi.com')
-  set.add('cls.ap-shenzhen-fsi.tencentcloudapi.com')
+  set.add("cls.tencentcloudapi.com");
+  set.add("cls.ap-guangzhou.tencentcloudapi.com");
+  set.add("cls.ap-shanghai.tencentcloudapi.com");
+  set.add("cls.ap-beijing.tencentcloudapi.com");
+  set.add("cls.ap-chengdu.tencentcloudapi.com");
+  set.add("cls.ap-chongqing.tencentcloudapi.com");
+  set.add("cls.ap-hongkong.tencentcloudapi.com");
+  set.add("cls.ap-singapore.tencentcloudapi.com");
+  set.add("cls.ap-bangkok.tencentcloudapi.com");
+  set.add("cls.ap-seoul.tencentcloudapi.com");
+  set.add("cls.ap-tokyo.tencentcloudapi.com");
+  set.add("cls.na-ashburn.tencentcloudapi.com");
+  set.add("cls.na-siliconvalley.tencentcloudapi.com");
+  set.add("cls.eu-frankfurt.tencentcloudapi.com");
+  set.add("cls.ap-shanghai-fsi.tencentcloudapi.com");
+  set.add("cls.ap-shenzhen-fsi.tencentcloudapi.com");
 
   await writeFile({ ...meta, content: Array.from(set).join("\n") });
 }
